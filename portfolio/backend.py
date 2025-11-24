03792341f0cb3ee90b0f86fe4acfea200b79d120
joserigel <joserigel18@gmail.com>
2025-11-24 14:20:35 +0100

    added connection to alpaca

--- portfolio/backend.py
+++ portfolio/backend.py
@@ -1,33 +1,43 @@
-import requests as req
 import os 
-import json
-import random 
-
 from flask import request
+
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from alpaca.data.historical import StockHistoricalDataClient
+
+from datetime import datetime, timedelta
+
 from __main__ import app
 
 @app.route("/api/portfolio", methods=["POST"])
 def portfolio():
     content = request.json
     
-    ticker_data = dict()
-    for ticker in set(map(lambda x: x["security"]["ticker"], content["transactions"])):
-        ticker = ticker.split(".")[0]
-        # url = f"https://api.massive.com/v2/snapshot/locale/us/markets/stocks/tickers/{ticker}" + \
-        #     f"?apiKey={os.getenv("MASSIVE_API_KEY")}"
-        # response = req.get(url)
-        # response.raise_for_status()
-        ticker_data[ticker] = round(random.random() * 200.0, 2)
-    ticker_data
-
+    # Init Alpaca Client
+    client = StockHistoricalDataClient(
+        os.getenv("APCA_API_KEY_ID"),
+        os.getenv("APCA_API_SECRET_KEY")
+    )
+    tickers = set(map(lambda x: x["security"]["ticker"].split(".")[0], content["transactions"]))
+    
+    # Fetch from API
+    req = StockBarsRequest(
+        symbol_or_symbols=list(tickers),
+        timeframe=TimeFrame.Day,
+        start=datetime.now() - timedelta(days=10),
+    )
+    ticker_data = client.get_stock_bars(req)
+    
+    # Transform to JSON
     purchase_transactions = filter(lambda x: x["type"] =="PURCHASE", content["transactions"])
     portfolio_data = []
     for purchase in purchase_transactions:
         ticker = purchase["security"]["ticker"].split(".")[0]
         portfolio_data.append({
             "stock": purchase["security"]["name"],
-            "current": ticker_data[ticker],
+            "current": ticker_data.data[ticker][-1].open,
             "shares": purchase["shares"],
-            "asset_value": purchase["shares"] * ticker_data[ticker]
+            "asset_value": purchase["shares"] * ticker_data.data[ticker][-1].open
         })
     return portfolio_data
