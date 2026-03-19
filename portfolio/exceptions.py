b1da1f40d7313bc0b6b9ae51abab8347044737b5
arashmdp <arashmdp@gmail.com>
2026-03-19 22:09:18 +0100

    add exceptions (TickerException, TradingDayException, DateException) and created tests for them, need the APCA credentials for failed tests

---
+++ portfolio/exceptions.py
@@ -0,0 +1,11 @@
+class TickerException(Exception):
+    """Raised when the ticker is not in the traded market"""
+    pass
+
+class TradingDayException(Exception):
+    """Raised when the ticker is not in the traded market"""
+    pass
+
+class DateException(Exception):
+    """Raised when the ticker is not in the traded market"""
+    pass
